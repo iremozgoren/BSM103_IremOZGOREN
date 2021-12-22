293b8447a7a41fc24451587988ca9d4d9594c909
iremozgoren <20010310052@ogrenci.bartin.edu.tr>
2021-12-22 22:52:49 +0300

    Week12
    Co-Authored-By: evrimguler <26477098+evrimguler@users.noreply.github.com>
    Co-Authored-By: fayaz44 <91744361+fayaz44@users.noreply.github.com>

---
+++ Week12/uygulama_3.java
@@ -0,0 +1,26 @@
+package calısma;
+
+import java.util.Scanner;
+
+public class uygulama_3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Bir kelime giriniz: ");
+		String kelime = sc.nextLine();
+		System.out.println("Girilen kelime = "+kelime);
+		
+		System.out.println("Kelimenin tersten girilmiş hali ="+tersten(kelime));
+	
+
+	}
+	public static String tersten(String kelime) {
+		String ters = "";
+		for(int index = kelime.length(); index > 0; index--) {
+			ters += kelime.substring(index-1, index);
+		}
+		return ters;
+	}
+
+}
