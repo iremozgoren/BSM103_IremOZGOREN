f55c7b6af9dcb43964ba3625621a1edbad340eb1
iremozgoren <91744503+iremozgoren@users.noreply.github.com>
2022-06-02 01:00:29 +0300

    Add files via upload

---
+++ Week10_BaharDonemi/src/main.java
@@ -0,0 +1,23 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		LinkedIntList linkList = new LinkedIntList();
+		linkList.add(5);
+		linkList.add(7);
+		linkList.add(9);
+		System.out.println(linkList.toString());
+		linkList.remove(2);
+		System.out.println(linkList.toString());
+		linkList.add(0, 11);
+		System.out.println(linkList.toString());
+		linkList.add(2, 15);
+		System.out.println(linkList.toString());
+		
+		int count = linkList.size();
+		System.out.println(count);
+		
+
+	}
+
+}
